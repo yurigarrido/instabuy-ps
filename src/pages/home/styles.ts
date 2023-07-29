1327d10f90baad0e60d4b97401c437a95f3a0137
yurigarrido <y.garrido.yuri@gmail.com>
2023-07-29 15:02:08 -0300

    feat: add link on carrousel container to view more products for clicked category

--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -22,10 +22,29 @@ export const Heading = styled('div', {
   display: 'flex',
   marginBottom: '$10',
   alignItems: 'center',
+  justifyContent: 'space-between',
   gap: '$4',
+
+  width: '100%',
 
   svg: {
     color: '$orange',
+  },
+
+  div: {
+    display: 'flex',
+    gap: '$4',
+    alignItems: 'center',
+
+    a: {
+      color: '$orange',
+      fontWeight: 'bold',
+      textDecoration: 'none',
+
+      '&:hover': {
+        textDecoration: 'underline',
+      },
+    },
   },
 })
 
@@ -37,7 +56,7 @@ export const CarrouselContainer = styled('div', {
   width: '100%',
 
   [` > ${Heading}`]: {
-    paddingLeft: '$8',
+    padding: '0 $8',
   },
 })
 
